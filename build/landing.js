6c10e3830aa1ab1456621e54d59f819482f5353b
migl3548 <thesnowcone101@gmail.com>
2025-03-01 22:17:24 -0700

    removed dumb stuff

--- build/landing.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const enterButton = document.getElementById('enter-app-button');
-    
-    enterButton.addEventListener('click', function() {
-        // Navigate to the React app
-        window.location.href = './index.html';
-    });
-});
